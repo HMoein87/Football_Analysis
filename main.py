44faa4543527cff7d508c005271105d1d79fefeb
hossein <hosseinmoeini87@gmail.com>
2024-08-23 15:10:54 -0300

    added tracker and drew players ellipses

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 
 def main():
     
     # Read the video file
     video_frames = read_video('input_video/08fd33_4.mp4')
+    
+    # Initialize tracker
+    tracker = Tracker("models/best.pt")
+    
+    # Get object tracks
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stubs= True,
+                                       stub_path='stubs/track_stubs.pkl')
+
+    # Draw output
+    ## Draw object tracks
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
     # Save the video file
-    save_video(video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
 
 if __name__ == '__main__':
---
+++ trackers/tracker.py
@@ -0,0 +1,195 @@
+from ultralytics import YOLO
+import supervision as sv
+import pickle
+import os
+import cv2
+import sys
+
+sys.path.append('../')
+from utils import get_bbox_center, get_bbox_width
+
+
+class Tracker:
+
+  def __init__(self, model_path):
+    '''
+    Initializes the tracker
+    
+    Args:
+      model_path: path to the model
+    '''
+    self.model = YOLO(model_path)
+    self.tracker = sv.ByteTrack()
+
+  def detect_frames(self, frames):
+    '''
+    Detects objects in a frames
+    
+    Args:
+      frames: list of frames
+    Returns
+      detections: list of detections
+    '''
+    
+    batch_size = 20
+    detections = []
+    for i in range(0, len(frames), batch_size):
+      batch = frames[i:i+batch_size]
+      batch_detections = self.model.predict(batch, conf=0.1)
+      detections += batch_detections
+      
+    # Return detections
+    return detections
+
+
+  def get_object_tracks(self, frames, read_from_stubs=False, stub_path=None):
+    '''
+    Gets object tracks from frames
+
+    Args:
+      frames: list of frames
+      read_from_stubs: whether to read from stubs
+      stub_path: path to the stub
+    Returns
+      tracks: dictionary of tracks
+    '''
+    
+    # Check if there is pickle file 
+    if read_from_stubs and stub_path is not None and os.path.exists(stub_path):
+      with open(stub_path, 'rb') as f:
+        tracks = pickle.load(f)
+        
+        return tracks
+
+    # Detect objects
+    detections = self.detect_frames(frames)
+    
+    # Initialize tracks
+    tracks = {
+        "players": [],
+        "referees": [],
+        "ball": []
+    }
+
+    # Iterate over frames
+    for frame_num, detection in enumerate(detections):
+      # Get class names
+      cls_names = detection.names
+      # Invert class names
+      cls_names_inverse = {v: k for k, v in cls_names.items()}
+
+      # Convert detections to supervision
+      detection_supervision = sv.Detections.from_ultralytics(detection)
+      
+      # Convert goalkeeper to player
+      for object_ind, class_id in enumerate(detection_supervision.class_id):
+        if cls_names[class_id] == 'goalkeeper':
+          detection_supervision.class_id[object_ind] = cls_names_inverse['player']
+      
+    # Track detections
+      detection_with_tracks = self.tracker.update_with_detections(
+        detection_supervision
+      )
+
+      # Format tracks in dictionary
+      tracks['players'].append({})
+      tracks['referees'].append({})
+      tracks['ball'].append({})
+      
+      # Iterate over tracks
+      for frame_detection in detection_with_tracks:
+        # Get bbox, class id and track id
+        bbox = frame_detection[0].tolist()
+        cls_id = frame_detection[3]
+        track_id = frame_detection[4]
+        
+        # Add track to dictionary
+        if cls_id == cls_names_inverse['player']:
+          tracks['players'][frame_num][track_id] = {'bbox':bbox}
+          
+        if cls_id == cls_names_inverse['referee']:
+          tracks['referees'][frame_num][track_id] = {'bbox':bbox}
+      
+      # Iterate over detections
+      for frame_detection in detection_supervision:
+        # Get bbox and class id
+        bbox = frame_detection[0].tolist()
+        cls_id = frame_detection[3]
+        
+        # Add ball detection to dictionary
+        if cls_id == cls_names_inverse['ball']:
+          tracks['ball'][frame_num][1] = {'bbox':bbox}
+    
+    # Save tracks to pickle file
+    if stub_path is not None:
+      with open(stub_path, 'wb') as f:
+        pickle.dump(tracks, f)
+        
+          
+    return tracks
+  
+  def draw_ellipse(self, frame, bbox, color, track_id):
+    '''
+    Draws an ellipse on the frame
+
+    Args:
+      frame: frame to draw on
+      bbox: bounding box
+      color: color of the ellipse
+      track_id: track id
+    Returns
+      frame: frame with the ellipse
+    '''
+
+    # Ellipse coordinates
+    y2 = int(bbox[3])
+    x_center, _ = get_bbox_center(bbox)
+    width = get_bbox_width(bbox)
+    
+    # Draw ellipse
+    cv2.ellipse(
+      frame,
+      center = (x_center, y2),
+      axes = (int(width), int(0.3*width)),
+      angle = 0,
+      startAngle = -45,
+      endAngle = 235,
+      color = color,
+      thickness = 2,
+      lineType = cv2.LINE_4
+    )
+    
+    return frame
+  
+  
+  def draw_annotations(self, video_frames, tracks):
+    '''
+    Draws annotations on the frames
+
+    Args:
+      video_frames: list of frames
+      tracks: dictionary of tracks
+    Returns
+      output_video_frames: list of frames with annotations
+    '''
+
+    # Initialize output video frames
+    output_video_frames = []
+    
+    # Iterate over frames
+    for frame_num, frame in enumerate(video_frames):
+      frame = frame.copy()
+      
+      # Get players, referees and ball dictionaries
+      player_dict = tracks["players"][frame_num]
+      referee_dict = tracks["referees"][frame_num]
+      ball_dict = tracks["ball"][frame_num]
+      
+      # Draw players
+      for track_id, player in player_dict.items():
+        frame = self.draw_ellipse(frame, player["bbox"], (0,0,255), track_id)
+        
+      
+      output_video_frames.append(frame)
+      
+    return output_video_frames
